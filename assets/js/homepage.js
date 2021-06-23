373431fbf9d156b2d11fa3bb1621f9c08e01a93b
JAMart <jorge.martinez1597@gmail.com>
2021-06-22 22:16:50 -0400

    added ability to use buttons to search for repos with issues needing specific languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,21 @@ var userFormEl = document.querySelector("#user-form");
 var userInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) { 
+                displayRepos(data.items, language);
+             });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -95,5 +110,19 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content 
+        repoContainerEl.textContent="";
+    }
+
+}
+
+
+languageButtonEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
